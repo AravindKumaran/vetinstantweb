4a8a3300b0e942b3cadeb923a57f1dca46748654
avinashvintek <avinash@vinteksolution.com>
2021-03-31 13:15:58 +0530

    Admin doctor details

--- src/pages/admin/DashBoard.js
+++ src/pages/admin/DashBoard.js
@@ -4,18 +4,30 @@ import LoadingSpinner from 'components/shared/UI/LoadingSpinner'
 import client from 'services/client'
 import toast from 'react-hot-toast'
 import { useTable } from 'react-table'
+import Backdrop from 'components/shared/UI/Backdrop'
+import Modal from 'components/shared/UI/Modal'
 
 import './DashBoard.css'
 
 const DashBoard = () => {
   const [allDoctors, setAllDoctors] = useState([])
   const [loading, setLoading] = useState(false)
+  const [open, setOpen] = useState(false)
+  const [currentDoc, setCurrentDoc] = useState('')
 
   const columns = useMemo(
     () => [
       {
         Header: 'Name',
         accessor: 'user.name',
+        Cell: ({ row: { original } }) => (
+          <p
+            className='doc__name__link'
+            onClick={() => handleShowDoctor(original)}
+          >
+            {original.user.name}
+          </p>
+        ),
       },
       {
         Header: 'Email',
@@ -70,7 +82,18 @@ const DashBoard = () => {
     getAllDoctors()
   }, [])
 
+  const toggleModal = () => {
+    setOpen(!open)
+  }
+
+  const handleShowDoctor = (doc) => {
+    setCurrentDoc(doc)
+    toggleModal()
+  }
+
   const handleBlock = async (doc) => {
+    console.log('Doc', doc)
+    return
     doc.user.block = !doc.user.block
     try {
       setLoading(true)
@@ -104,6 +127,72 @@ const DashBoard = () => {
   return (
     <Layout>
       {loading && <LoadingSpinner asOverlay />}
+      {open && <Backdrop toggle={toggleModal} />}
+      {open && (
+        <Modal
+          toggle={toggleModal}
+          title={`Dr. ${currentDoc.user.name} Details`}
+        >
+          {currentDoc && (
+            <div className='doctor'>
+              <h4>Details: </h4>
+              <div className='doctor__card'>
+                <h5>Registration Number :</h5>
+                <p>{currentDoc.regNo}</p>
+              </div>
+              <div className='doctor__card'>
+                <h5>Phone Number:</h5>
+                <p>{currentDoc.phone}</p>
+              </div>
+              <div className='doctor__card'>
+                <h5>Hospital/Clinic Name :</h5>
+                <p>{currentDoc?.hospital?.name}</p>
+              </div>
+              <div className='doctor__card'>
+                <h5>Consultation Fees :</h5>
+                <p>₹ {currentDoc.fee}</p>
+              </div>
+              <div className='doctor__card'>
+                <h5> Qualifications :</h5>
+                <p>{currentDoc.qlf}</p>
+              </div>
+
+              {+currentDoc.fee > 0 && (
+                <>
+                  <h4>Billing Details: </h4>
+                  <div className='doctor__card'>
+                    <h5>Bank Account Number :</h5>
+                    <p>{currentDoc.accno}</p>
+                  </div>
+                  <div className='doctor__card'>
+                    <h5>Account Holder Name :</h5>
+                    <p>{currentDoc.accname}</p>
+                  </div>
+                  <div className='doctor__card'>
+                    <h5>Account Type :</h5>
+                    <p>{currentDoc.acctype}</p>
+                  </div>
+                  <div className='doctor__card'>
+                    <h5>IFSC Code :</h5>
+                    <p>{currentDoc.ifsc}</p>
+                  </div>
+                </>
+              )}
+
+              <div className='doctor__card'>
+                <h5>Registration Certificate :</h5>
+                <a
+                  style={{ cursor: 'pointer' }}
+                  href={currentDoc.file}
+                  target='_blank'
+                >
+                  {currentDoc.file.split('/documents/')[1]}
+                </a>
+              </div>
+            </div>
+          )}
+        </Modal>
+      )}
       <div className='center'>
         <h1 className='py-10'>All Doctors</h1>
         <table {...getTableProps()}>
