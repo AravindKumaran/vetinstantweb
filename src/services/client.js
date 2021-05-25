fc1a42edf4d1e8f0a7c4ac4e81ed460398da2697
Tamilvanan25 <tamilvanan@vinteksolution.com>
2021-05-25 19:25:32 +0530

    Changed URL to https

--- src/services/client.js
+++ src/services/client.js
@@ -4,7 +4,7 @@ const client = axios.create({
   // baseURL: 'http://192.168.43.242:8000/api/v1',
   // baseURL: 'https://vetinstantbe.azurewebsites.net/api/v1',
   baseURL:
-    "http://ec2-13-126-97-116.ap-south-1.compute.amazonaws.com:8000/api/v1",
+    "https://ec2-13-126-97-116.ap-south-1.compute.amazonaws.com:8000/api/v1",
 });
 
 export default client;
